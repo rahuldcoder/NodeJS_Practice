befd11b0d955b2c9579ce5b16151c9dbf430c8f7
rahuldcoder <rahulthakur02k@gmail.com>
2017-10-30 19:41:43 +0530

    Implemented asynchronous programming using callback functions

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ var rect = require('./rectangle');
 
 function solveRect(l,b){
     console.log("Solving for rectangle with l= "+l+" breadth= "+b);
-    if(l<=0 ||b <= 0){
-        console.log("Rectangle's dimensions are negative");
-    }
-    else
-    {   
-        console.log("Perimeter of x = "+ rect.perimeter(l,b));
-        console.log("Area of rectangle = "+rect.area(l,b));
-    }
+    rect (l,b,(err,rectangle) =>{
+        if(err)
+        {
+            console.log("Error:",err.message);
+        }
+        else
+        {
+            console.log("Area of Rectangle of dimensions l ="+l+" and b= "+b+" is "+rectangle.area());
+            console.log("Perimeter of Rectangle of dimensions l ="+l+" and b= "+b+" is "+rectangle.perimeter());
+        }
+    });
+    console.log("This is after call to rect");
 }
 solveRect(2,4);
